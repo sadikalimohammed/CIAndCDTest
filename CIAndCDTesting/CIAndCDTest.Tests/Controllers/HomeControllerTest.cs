f804f576f81d132f3b2c9a8e7563e87d3ea580d0
410490 <sadik.ali@toyota.com>
2017-10-12 11:13:59 -0400

    Web config Changes

--- CIAndCDTesting/CIAndCDTest.Tests/Controllers/HomeControllerTest.cs
+++ CIAndCDTesting/CIAndCDTest.Tests/Controllers/HomeControllerTest.cs
@@ -35,7 +35,7 @@ namespace CIAndCDTest.Tests.Controllers
             ViewResult result = controller.About() as ViewResult;
 
             // Assert
-            Assert.AreEqual("Your application description page.", result.ViewBag.Message + "Fail");
+            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
         }
 
         [TestMethod]
